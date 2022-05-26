b0cad4a1066bfa9c00318a1a13382e53c39283b6
Lucas G <lucasgoe@gmail.com>
2022-05-25 21:27:06 -0600

    ReviewAndANew
    A new codwars and some fizzbuzz review

--- LearnWithLeonSub/fizzbuzz.js
+++ LearnWithLeonSub/fizzbuzz.js
@@ -5,8 +5,8 @@
 // e:
 
 // function fizzBuzz(n) {
-//     //loop
-//     //conditionals % 3&5, % 3, % 5
+//     // for loop
+//     //conditionals % 3&5 fizzbuzz, % 3 fizz, % 5 buzz
 //     for (var i = 1; i < 101; i++) {
 //         if (i % 15 == 0) console.log("FizzBuzz");
 //         else if (i % 3 == 0) console.log("Fizz");
@@ -15,4 +15,23 @@
 //     }
 // }
 
-fizzBuzz = n => new Array(n).fill(0).map((a, i) => (++i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || '' + i)
+
+// fizzBuzz = n => new Array(n).fill(0).map((a, i) => (++i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || '' + i)
+
+// Use an implicit return to solve fizzBuzz
+
+// call fizzBuzz for n
+// Create a new Array class from n
+// fill array n to separate it into an array of individual elements equal to the array length starting from zero
+// map the array using an accumulator and the index
+// creating a map structure that 
+// iterates the index ++i and 
+// returns an empty string if mod 3 is truthy, or fizz if mod 3 is falsy
+// plus
+// an empty string if mod 5 is truthy, or buzz if mod 5 is falsy 
+// or 
+// an empty string plus the index value 
+
+fizzBuzz = n => new Array(n).fill(0).map((a,i) => (++i % 3 ? '' : 'Fizz') + (i % 5 ? '' : 'Buzz') || '' + i)
+
+console.log(fizzBuzz(26))
